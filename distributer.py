6e162079484c6245acc37487bee3655cfa361005
Ben Cohen <bn.chn2@gmail.com>
2014-02-22 18:29:21 -0500

    new fun

--- distributer.py
+++ distributer.py
@@ -3,24 +3,28 @@ from socket import *
 import thread
 
 import json
-totals = {chr(x + 97): 0 for x in range(26)}
+totals = {}
 
 class job_list(object):
     def __init__(self, text_file, num_jobs):
-        flat_text = open(text_file).readline()
+        # flat_text = open(text_file).readline()
 
-        length = len(flat_text)
-        allJobs = []
+        # length = len(flat_text)
+        # allJobs = []
 
-        for x in range(num_jobs):
+        # for x in range(num_jobs):
             
-            allJobs.append(flat_text[x * (length/num_jobs):(x+1) * (length/num_jobs)])
+        #     allJobs.append(flat_text[x * (length/num_jobs):(x+1) * (length/num_jobs)])
 
 
-        self.allJobs = allJobs
+        # self.allJobs = allJobs
+        # self.num = 0
+        # self.jobs = len(allJobs)
+        # print "CREATED WITH " + str(length/128) + " jobs"
+
+        self.allJobs = [x for x in range(1000,1050)]
         self.num = 0
-        self.jobs = len(allJobs)
-        print "CREATED WITH " + str(length/128) + " jobs"
+        self.jobs = len(self.allJobs)
 
 
     def __iter__(self):
@@ -52,11 +56,11 @@ def handler(clientsock,addr, jobs):
                 print "Giving out a job: " + str(jobs_sent)
                 jobs_sent += 1
                 next_job = jobs.next_job()
+                print "NJ: " + str(next_job)
                 if next_job == -1:
                     clientsock.sendall('done');
                     print totals
                     return 
-                print "SUCESSFULLY GAVE OUT JOB"
                 clientsock.sendall(str(next_job))
                 try:
                     data = clientsock.recv(BUFFER_SIZE)
@@ -67,13 +71,14 @@ def handler(clientsock,addr, jobs):
                         data = clientsock.recv(BUFFER_SIZE)
                     except:
                         pass
-                # print data
-                data = json.loads(data)
+                print data
+                totals[next_job] = data
+               
 
                 for x in data:
                     totals[x] += data[x]
-            except:
-                pass
+        except:
+            pass
 
 
 def main():
